dcf1c734fde34e86a5246d2cf05fc25d0b55934b
Kacper Perzankowski <perzankowski.kacper@gmail.com>
2019-05-28 13:45:13 +0200

    test

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from './rollup-plugins/typescript-export';
+
+export default {
+  input: 'index.js',
+  output: [{
+      file: 'main.js',
+      format: 'cjs'
+    },
+    {
+      file: 'main.es.js',
+      format: 'es'
+    }
+  ],
+  plugins: [typescript()]
+}
